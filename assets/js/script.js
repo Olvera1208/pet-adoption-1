58435fdff72598890bbe271a055cb85b00909931
Fauzia Shafi <fauziashafi12@gmail.com>
2022-01-16 15:36:47 -0800

    Adding final script.js code with jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,42 @@
-console.log("document ready")
-var submitBtn = document.querySelector("#submit-btn");
-var searchInput = document.querySelector("#searchInput");
-var animalType = document.querySelector("#type");
-var size = document.querySelector("#size");
 
+var animalTypeEl = $("#type");
+var genderEl = $("#gender");
 var cityArray = [];
-
 console.log("document is ready");
 
 function getUserInput() {
-    console.log("Submit btn is clicked");
 
-    var searchedLocation = searchInput.value;
+    var searchedLocation = $("#searchInput").val();
     console.log(searchedLocation);
     
-    var animalType = animalType.value;
+    var animalType = animalTypeEl.val();
     console.log(animalType);
 
-    // var animalSize = size.value;
-    // console.log(animalSize);
+    var animalSize = $("#size").val();
+    console.log(animalSize);
+
+    var animalGender = genderEl.val();
+    console.log(animalGender);
 
     // store into local storage
-    // cityArray.push(searchedLocation);
-    // localStorage.setItem("nameOfCity", JSON.stringify(cityArray));
-
-    // var animalType = animalType.value;
-    // console.log(animalType);
+    cityArray.push(searchedLocation);
+    localStorage.setItem("nameOfCity", JSON.stringify(cityArray));
 
     if (!searchedLocation) {
         console.error('You need a search input value!');
         return;
     }
+    
+    var resultPageURL = './listings.html?q=' + searchedLocation + '&format=' + animalType + '&format=' + animalSize + '&format=' + animalGender ; 
+    console.log(resultPageURL); 
 
-    // var resultPageURL = './listings.html?q=' + searchedLocation + '&format=' + animalType + '&format=' ; 
-
-    // var resultPageURL = './listings.html';
-    // location.assign(resultPageURL);
+    location.assign(resultPageURL);
 
 
 }
 
-submitBtn.addEventListener("click", function (event) {
+$("#submit-btn").on("click", function (event) {
+    event.preventDefault();
     getUserInput();
 
 })
